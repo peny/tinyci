0987ee947013b761026a3edd6729c97553cf9fd5
Petter Nyman <petter@pttr.se>
2014-01-24 21:09:24 +0100

    use default http module for now

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-var koa = require('koa');
-var app = koa();
+var http = require('http');
+var spawn = require('child_process').spawn;
 
-app.use(function *(){
-    this.body = 'Hello World';
+var server = http.createServer(function (request, response) {
+  if(request.url.match(/ci/)){
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("OK\n");
+    testCommit();
+  } else {
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.end("Not found\n");
+  }
 });
 
-app.listen(3000);
+function testCommit(repo){
+  if(!repo){
+    repo = process.env.TINYCI_DEFAULT_REPO;
+  }
+  spawn('git clone '+repo);
+
+}
+
+server.listen(3000);
